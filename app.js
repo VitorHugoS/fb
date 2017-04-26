adbad5a8324157bf07f71ac9c65d595facbf65b8
vitorhugos <vitorhugoss17@gmail.com>
2017-04-26 17:32:35 -0300

    Banco de dados

--- app.js
+++ app.js
@@ -143,12 +143,11 @@ app.get('/authorize', function(req, res) {
 });
 
 app.get('/banco', function(req, res) {
+      var bodys; 
       request('http://www.google.com', function (error, response, body) {
-        console.log('error:', error); // Print the error if one occurred 
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
-        console.log('body:', body); // Print the HTML for the Google homepage. 
+        bodys=body; 
       });
-      res.send("new app");
+      res.write(bodys);
 });
 
 /*
