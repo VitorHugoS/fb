509a1e0bfd97c1a19624fdb928b0309abe2dbab3
vitorhugos <vitorhugoss17@gmail.com>
2017-04-28 09:47:11 -0300

    Banco dados 2

--- app.js
+++ app.js
@@ -161,7 +161,7 @@ app.get('/login', function(req, res) {
         {
                 res.writeHead(200, { 'Content-Type': 'application/json'});
                 res.end(JSON.stringify(rows));
-                res.end();
+                //res.end();
         }); 
 });
 
