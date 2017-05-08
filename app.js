1305ff78e65374a0dbaabbbbe3767b1856a4c7a9
vitorhugos <vitorhugoss17@gmail.com>
2017-05-08 10:59:44 -0300

    menu persistente

--- app.js
+++ app.js
@@ -932,8 +932,8 @@ function startConversation(recipientId) {
     json: {}}, function (error, response, body) {
       dados=JSON.stringify(response);
       console.log(dados);
-      nome=response.first_name;
-      sobrenome=response.last_name;
+      nome=dados.body.first_name;
+      sobrenome=dados.body.last_name;
   });  
   var messageData = {
     recipient: {
