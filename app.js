4c330aa02e53c2dbfbf1c1097b903d1fda2f5f13
vitorhugos <vitorhugoss17@gmail.com>
2017-05-09 17:22:09 -0300

    menu persistente

--- app.js
+++ app.js
@@ -345,7 +345,7 @@ function receivedMessage(event) {
   var messageText = message.text;
   var messageAttachments = message.attachments;
   var quickReply = message.quick_reply;
-
+console.log(_estado[senderID]);
 if( _estado[senderID]==!undefined || _estado[senderID]==!null){
 
   switch(_estado[senderID]){
