9d0c2d34e99df9be591f8ef1a9c93b1c473ae65c
vitorhugos <vitorhugoss17@gmail.com>
2017-05-10 15:43:51 -0300

    tratamento delivery

--- app.js
+++ app.js
@@ -251,7 +251,8 @@ app.get('/config3', function(req, res) {
 app.get('/config4', function(req, res) {
   connection.query('SELECT * from `sessaoUser` WHERE `idUser` =22 limit 1', [], function(err, rows, fields)
         { 
-          if(rows===0){
+          console.log(rows.length);
+          if(rows){
             connection.query('insert into sessaoUser (idUser, status) values ("22", "validando")', []);  
           }else{
             connection.query('UPDATE sessaoUser set status="validando" WHERE `idUser` = 22', []);
