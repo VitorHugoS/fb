637761f99d4951db6ae68d9cc7198e5997fc4a4c
vitorhugos <vitorhugoss17@gmail.com>
2017-05-10 16:26:57 -0300

    tratamento delivery

--- app.js
+++ app.js
@@ -251,7 +251,7 @@ app.get('/config3', function(req, res) {
 app.get('/config4', function(req, res) {
    connection.query('SELECT * from `sessaoUser` WHERE `idUser` ="1677684085581113" limit 1', [], function(err, rows, fields)
         { 
-          console.log(rows);
+          console.log(rows[0].status);
           //if(rows.length!=0){
           //  
           //}else{
